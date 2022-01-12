ed59fc7d2d848c89464643f784096b41a0e9285e
skydoves <skydoves@naver.com>
2022-01-13 08:26:57 +0900

    Update spotless License configurations

--- avatarview/src/main/kotlin/io/getstream/avatarview/AvatarShape.kt
+++ avatarview/src/main/kotlin/io/getstream/avatarview/AvatarShape.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.getstream.avatarview
 
 /** An attribute type of [AvatarView] to determine the shape of the image and border. */
