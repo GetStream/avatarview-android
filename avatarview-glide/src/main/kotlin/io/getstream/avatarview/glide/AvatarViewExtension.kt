2282c99db8ca63e7e2d91904c304657e5bc19f91
skydoves <skydoves@naver.com>
2022-01-13 08:24:48 +0900

    Update spotless License configurations

--- avatarview-glide/src/main/kotlin/io/getstream/avatarview/glide/AvatarViewExtension.kt
+++ avatarview-glide/src/main/kotlin/io/getstream/avatarview/glide/AvatarViewExtension.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Stream.IO, Inc. All Rights Reserved.
+ * Copyright 2022 Stream.IO, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.getstream.avatarview.glide
 
 import android.graphics.Bitmap
